15c5fb05e67bf1d7eeee32468bb1e4375376e9d0
James51332 <jamesddadabo@gmail.com>
2021-02-28 17:48:16 -0600

    Refactored Array Memory Project

--- arraymemory/main.cpp
+++ arraymemory/main.cpp
@@ -1,36 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
-// Create a pointer to the base of our array
-float* array = new float[5];
+  // Create a pointer to the base of our array
+  float *array = new float[5];
 
-// Create a pointer to our access point in the array
-float* access = array;
+  // Create a pointer to our access point in the array
+  float *access = array;
 
-// Log info about our arrays
-cout << "Created array with size 5 at address " << array << endl;
-cout << "Create access pointer at address " << access << endl;
+  // Log info about our arrays
+  cout << "Created array with size 5 at address " << array << endl;
+  cout << "Created access pointer at address " << access << endl;
 
-// Set the first values of the array to 1 and 4
-(*access) = 1;
-access++; // increments the value sizeof(float)
-(*access) = 4;
+  // Point Access to next element in array
+  access++; // increments the value sizeof(float)
 
-// Log changing values
-cout << "Moved access to address " << access << endl;
+  // Log changing values
+  cout << "Moved access to address " << access << endl;
 
-// Log expected pointer changes
-cout << "The expected change to access is sizeof(float) which is " << sizeof(float) << endl;
+  // Log expected pointer changes
+  cout << "The expected change to access is sizeof(float) which is " << sizeof(float) << endl;
 
-// Log actual pointer changes
-uint32_t change = static_cast<uint32_t>((uint8_t*)access - (uint8_t*)array); // We cast the pointer to uint8_t because it has a size of one bite (if we don't, it returns 1 because the difference is measured in sizeof(float)'s.
+  // Log actual pointer changes
+  // We cast the pointer to uint8_t because it has a size of one byte
+  // If we don't, it returns 1 because the difference is measured in sizeof(float)'s.
+  // You could also find the total change in address by taking the difference and multipling
+  // by sizeof(float).
+  uint32_t change = static_cast<uint32_t>((uint8_t *)access - (uint8_t *)array);
 
-cout << "The actual change to access is access - array which is " << (uint32_t)((uint8_t*)access - (uint8_t*)array) << endl;
+  cout << "The actual change to access is access - array which is " << (uint32_t)((uint8_t *)access - (uint8_t *)array) << endl;
 
-// Free the array
-access = nullptr;
-delete[] array;
+  // Free the array
+  access = nullptr;
+  delete[] array;
 
+  return 0;
 }
