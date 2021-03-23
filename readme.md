7f151a3938e46e2f6e6c585cf35effeedc07e0aa
James51332 <jamesddadabo@gmail.com>
2021-03-23 16:46:02 -0500

    Fixed memcpy on windows and updated readme

--- readme.md
+++ readme.md
@@ -1,7 +1,11 @@
-# Expirements
+# Experiments
 
 This is a compilation of projects I wrote to test different features in c++. They aren't necesarily useful but somebody may find them helpful.
 
 ## Getting Started
 
 Clone the repo and `cd` into the src directory. the makefile has the default project set to helloworld. run `make project=[Project]` to compile that project. It will output to the bin directory. `make run project=[Project]` will run it.
+
+## Contributing
+
+Contributions are welcome! If you have a concept that you know and want to share, feel free to submit a pull request. If you have a concept that you don't quite understand either, create an issue. 
--- resizearray/main.cpp
+++ resizearray/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
 #include <memory>
 using namespace std;
 
 // Step 0: Retrieve info about array (pass location by reference to change for user. this could be replicated using int** in c)
 void resize(int*& arrayLocation, int curSize, int newSize) {
   // Step 1: allocate new memory for our resized array
-  int* newArray = (int*) malloc(sizeof(int) * newSize);
+  int* newArray = (int*)malloc(sizeof(int) * newSize);
 
   // Step 2: copy the content of our old array into the new array
   int safeSize = curSize < newSize ? curSize : newSize; // figure out how much memory is safe for both arrays
@@ -21,8 +22,8 @@ void resize(int*& arrayLocation, int curSize, int newSize) {
 
 int main() {
   int size = 10;
-  int* array = (int*) malloc(sizeof(int) * size);
- 
+  int* array = (int*)malloc(sizeof(int) * size);
+
   cout << "Creating array with " << size << " elements." << endl;
   for (int i = 0; i < size; i++) {
     array[i] = i + 1;
@@ -33,14 +34,14 @@ int main() {
 
   int newSize = 12;
   cout << "Resizing array to " << newSize << " elements." << endl;
-  
+
   resize(array, size, newSize);
-  size = newSize; 
+  size = newSize;
   for (int i = 0; i < size; i++) {
     array[i] = i + 1;
     cout << array[i];
     if (i + 1 < size) cout << ", ";
-  } 
+  }
   cout << endl;
 
   free(array);
